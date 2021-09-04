611edb1da2cdc9c35b7955ad4c3a1255bbd005f6
danielsimas02 <83676199+danielsimas02@users.noreply.github.com>
2021-09-04 13:22:26 -0300

    Add files via upload
    Boa tarde, por enquanto não conseguimos efetuar a conexão com o banco de dados.

---
+++ HOW III/HOW III/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace HOW_III
+{
+	partial class Confirme
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tbTarefa1 = new System.Windows.Forms.CheckBox();
+            this.tbTarefa2 = new System.Windows.Forms.CheckBox();
+            this.tbTarefa3 = new System.Windows.Forms.CheckBox();
+            this.tbDescricao = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tbGravar = new System.Windows.Forms.Button();
+            this.tbReset = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dbDataSet = new HOW_III.dbDataSet();
+            this.dbDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descrição = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colcheck = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(573, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(231, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // tbTarefa1
+            // 
+            this.tbTarefa1.AutoSize = true;
+            this.tbTarefa1.Location = new System.Drawing.Point(582, 73);
+            this.tbTarefa1.Name = "tbTarefa1";
+            this.tbTarefa1.Size = new System.Drawing.Size(15, 14);
+            this.tbTarefa1.TabIndex = 1;
+            this.tbTarefa1.UseVisualStyleBackColor = true;
+            this.tbTarefa1.CheckedChanged += new System.EventHandler(this.tbTarefa1_CheckedChanged);
+            // 
+            // tbTarefa2
+            // 
+            this.tbTarefa2.AutoSize = true;
+            this.tbTarefa2.Location = new System.Drawing.Point(582, 105);
+            this.tbTarefa2.Name = "tbTarefa2";
+            this.tbTarefa2.Size = new System.Drawing.Size(15, 14);
+            this.tbTarefa2.TabIndex = 2;
+            this.tbTarefa2.UseVisualStyleBackColor = true;
+            // 
+            // tbTarefa3
+            // 
+            this.tbTarefa3.AutoSize = true;
+            this.tbTarefa3.Location = new System.Drawing.Point(582, 139);
+            this.tbTarefa3.Name = "tbTarefa3";
+            this.tbTarefa3.Size = new System.Drawing.Size(15, 14);
+            this.tbTarefa3.TabIndex = 3;
+            this.tbTarefa3.UseVisualStyleBackColor = true;
+            // 
+            // tbDescricao
+            // 
+            this.tbDescricao.Location = new System.Drawing.Point(635, 70);
+            this.tbDescricao.Name = "tbDescricao";
+            this.tbDescricao.Size = new System.Drawing.Size(169, 20);
+            this.tbDescricao.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(635, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(635, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // tbGravar
+            // 
+            this.tbGravar.Location = new System.Drawing.Point(573, 346);
+            this.tbGravar.Name = "tbGravar";
+            this.tbGravar.Size = new System.Drawing.Size(75, 23);
+            this.tbGravar.TabIndex = 7;
+            this.tbGravar.Text = "Gravar";
+            this.tbGravar.UseVisualStyleBackColor = true;
+            this.tbGravar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // tbReset
+            // 
+            this.tbReset.Location = new System.Drawing.Point(729, 346);
+            this.tbReset.Name = "tbReset";
+            this.tbReset.Size = new System.Drawing.Size(75, 23);
+            this.tbReset.TabIndex = 8;
+            this.tbReset.Text = "Reset";
+            this.tbReset.UseVisualStyleBackColor = true;
+            this.tbReset.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(635, 171);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(169, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(582, 174);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(635, 206);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(169, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(582, 209);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(635, 241);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(169, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(582, 244);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Data,
+            this.Descrição,
+            this.colcheck});
+            this.dataGridView1.DataSource = this.dbDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(369, 385);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(696, 180);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dbDataSet
+            // 
+            this.dbDataSet.DataSetName = "dbDataSet";
+            this.dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dbDataSetBindingSource
+            // 
+            this.dbDataSetBindingSource.DataSource = this.dbDataSet;
+            this.dbDataSetBindingSource.Position = 0;
+            // 
+            // Data
+            // 
+            this.Data.HeaderText = "colData";
+            this.Data.Name = "Data";
+            this.Data.Width = 150;
+            // 
+            // Descrição
+            // 
+            this.Descrição.HeaderText = "coldescricao";
+            this.Descrição.Name = "Descrição";
+            this.Descrição.Width = 400;
+            // 
+            // colcheck
+            // 
+            this.colcheck.HeaderText = "colCheck";
+            this.colcheck.Name = "colcheck";
+            // 
+            // Confirme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1441, 653);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tbReset);
+            this.Controls.Add(this.tbGravar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbDescricao);
+            this.Controls.Add(this.tbTarefa3);
+            this.Controls.Add(this.tbTarefa2);
+            this.Controls.Add(this.tbTarefa1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Confirme";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox tbTarefa1;
+        private System.Windows.Forms.CheckBox tbTarefa2;
+        private System.Windows.Forms.CheckBox tbTarefa3;
+        private System.Windows.Forms.TextBox tbDescricao;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button tbGravar;
+        private System.Windows.Forms.Button tbReset;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dbDataSetBindingSource;
+        private dbDataSet dbDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descrição;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colcheck;
+    }
+}
+
---
+++ HOW III/HOW III/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HOW_III
+{
+	public partial class Confirme : Form
+	{
+        private object dgConfirme;
+
+        public Confirme()
+		{
+			InitializeComponent();
+		}
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "tarefas";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void Confirme_Load(object sender, EventArgs e)
+        {
+            atualizaGrid();
+        }
+
+        private void atualizaGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM listatarefas";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgConfirme.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgConfirme.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//DESCRICAO
+                    row.Cells[3].Value = reader.GetString(3);//CATEGORIA
+                    row.Cells[4].Value = reader.GetString(4);//ANO
+                    dgConfirme.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbTarefa1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbTarefa4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbTarefa1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object clear, EventArgs e)
+        {
+            this.LimpaCampos(Controls);
+
+        }
+
+
+
+        public void LimpaCampos(Control.ControlCollection controles)
+        {
+            foreach (Control item in controles)
+            {
+                if (item.GetType() == typeof(TextBox))
+                {
+                    item.Text = string.Empty; //limpa todos os controles do tipo TextBox
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
